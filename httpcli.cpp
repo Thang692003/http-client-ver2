52ba568fc3e26e05f2c4a3e28f1bf2d11c820f25
Mrsteve23 <ngohoanglan2k3@gmail.com>
2023-11-27 10:58:43 +0700

    fix linh tinh
    add get, post

--- httpcli.cpp
+++ httpcli.cpp
@@ -1,9 +1,8 @@
 #include "httpcli.h"
-#include "httptcpclient.h"
 #include <iostream>
 
 
-HttpClientCLI::HttpClientCLI():CmdLineInterface("Http Client>")
+HttpClientCLI::HttpClientCLI():CmdLineInterface("Echo Client>")
 {
     cout << "Http Client version 2.0" << endl;
     cout << "Nhap lenh help de duoc tro giup" << endl;
@@ -20,19 +19,17 @@ void HttpClientCLI::initCmd()
    // add code here
    addCmd("open",CLI_CAST(&HttpClientCLI::doOpen));
    addCmd("close",CLI_CAST(&HttpClientCLI::doClose));
-   addCmd("attendace",CLI_CAST(&HttpClientCLI::doAttendace));
-   //addCmd("get",CLI_CAST(&EchoClientCLI::doGet));
-   addCmd("upload",CLI_CAST(&HttpClientCLI::doUpload));
-   addCmd("turnin",CLI_CAST(&HttpClientCLI::doTurnIn));
+   addCmd("echo",CLI_CAST(&HttpClientCLI::doEcho));
+   addCmd("get",CLI_CAST(&HttpClientCLI::doGet));
    addCmd("help",CLI_CAST(&HttpClientCLI::doHelp));
-   addCmd("quit",CLI_CAST(&HttpClientCLI::doQuit));
+   addCmd("post",CLI_CAST(&HttpClientCLI::doPost));
 }
 
 /*
     get [URL]
 */
 
-void HttpClientCLI::doOpen(string cmd_argv[], int cmd_argc)
+void HttpClientCLI::doGet(string cmd_argv[], int cmd_argc)
 {
     if(cmd_argc==2)
     {
@@ -60,57 +57,88 @@ void HttpClientCLI::doOpen(string cmd_argv[], int cmd_argc)
     }
 }
 
+/*
+    post [URL] <msv>
+*/
+
+void HttpClientCLI::doPost(string cmd_argv[], int cmd_argc)
+{
+    if(cmd_argc==3)
+    {
+       //add code here
+       bool ok = client.postHTTP(cmd_argv[1], cmd_argv[2]);
+       if(ok)
+       {
+           cout << "POST done!" << endl;
+       }
+    }
+    else if(cmd_argc==1)
+    {
+       string url, msv;
+       cout << "Nhap URL: ";
+       getline(cin, url);
+       cout << "Nhap MSV: ";
+       getline(cin, msv);
+       bool ok = client.postHTTP(url, msv);
+       if(ok)
+       {
+           cout << "POST done!" << endl;
+       }
+    }
+    else
+    {
+        cout << "Cu phap lenh khong chinh xac" << endl;
+    }
+}
 
 /*
    open <server> [port]
    open <server>
 
 */
-
-
-//void EchoClientCLI::doOpen(string cmd_argv[], int cmd_argc)
-//{
-//    if(cmd_argc==3)
-//    {
-//       //add code here
-//       if(client.isConnected())
-//       {
-//           cout << "Da co ket noi, hay dong ket noi lai." << endl;
-//           return;
-//       }
-//       bool ok = client.open(cmd_argv[1],cmd_argv[2]);
-//       if(ok)
-//       {
-//           cout << "Ket noi thanh cong" << endl;
-//       }
-//       else
-//       {
-//           cout << "Ket noi KHONG thanh cong" << endl;
-//       }
-//    }
-//    else if(cmd_argc==2)
-//    {
-//       // add code here
-//       if(client.isConnected())
-//       {
-//           cout << "Da co ket noi, hay dong ket noi lai." << endl;
-//           return;
-//       }
-//       bool ok = client.open(cmd_argv[1],"echo");
-//       if(ok)
-//       {
-//           cout << "Ket noi thanh cong" << endl;
-//       }
-//       else
-//       {
-//           cout << "Ket noi KHONG thanh cong" << endl;
-//       }
-//    }
-//    else
-//    {
-//        cout << "Cu phap lenh khong chinh xac" << endl;
-//    }
-//}
+void HttpClientCLI::doOpen(string cmd_argv[], int cmd_argc)
+{
+    if(cmd_argc==3)
+    {
+       //add code here
+       if(client.isConnected())
+       {
+           cout << "Da co ket noi, hay dong ket noi lai." << endl;
+           return;
+       }
+       bool ok = client.open(cmd_argv[1],cmd_argv[2]);
+       if(ok)
+       {
+           cout << "Ket noi thanh cong" << endl;
+       }
+       else
+       {
+           cout << "Ket noi KHONG thanh cong" << endl;
+       }
+    }
+    else if(cmd_argc==2)
+    {
+       // add code here
+       if(client.isConnected())
+       {
+           cout << "Da co ket noi, hay dong ket noi lai." << endl;
+           return;
+       }
+       bool ok = client.open(cmd_argv[1],"echo");
+       if(ok)
+       {
+           cout << "Ket noi thanh cong" << endl;
+       }
+       else
+       {
+           cout << "Ket noi KHONG thanh cong" << endl;
+       }
+    }
+    else
+    {
+        cout << "Cu phap lenh khong chinh xac" << endl;
+    }
+}
 
 void HttpClientCLI::doClose(string cmd_argv[], int cmd_argc)
 {
@@ -119,65 +147,46 @@ void HttpClientCLI::doClose(string cmd_argv[], int cmd_argc)
     cout << "Da dong ket noi." << endl;
 }
 
-void HttpClientCLI::doQuit(string cmd_argv[], int cmd_argc)
-{
-    cout << "Quit" << endl;
-}
-
-void HttpClientCLI::doTurnIn(string cmd_argv[], int cmd_argc)
-{
-    cout << "Quit" << endl;
-}
-
-void HttpClientCLI::doUpload(string cmd_argv[], int cmd_argc)
-{
-    cout << "Quit" << endl;
-}
-
-void HttpClientCLI::doAttendace(string cmd_argv[], int cmd_argc)
-{
-    cout << "Quit" << endl;
-}
-
 /*
     echo  [msg]
     echo
 */
 
-//void HttpClientCLI::doEcho(string cmd_argv[], int cmd_argc)
-//{
-//    if(cmd_argc==2)
-//    {
-//        // add code here
-//        bool ok = client.echo(cmd_argv[1]);
-//        if(ok)
-//            cout << "Echo thanh cong." << endl;
-//    }
-//    else if(cmd_argc == 1)
-//    {
-//        // add code here
-//        string msg;
-//        cout << "Nhap ban tin:";
-//        getline(cin, msg);
-//        bool ok = client.echo(msg);
-//        if(ok)
-//            cout << "Echo thanh cong." << endl;
-//    }
-//    else
-//    {
-//        cout << "Invalid arguments" << endl;
-//    }
-//}
+void HttpClientCLI::doEcho(string cmd_argv[], int cmd_argc)
+{
+    if(cmd_argc==2)
+    {
+        // add code here
+        bool ok = client.echo(cmd_argv[1]);
+        if(ok)
+            cout << "Echo thanh cong." << endl;
+    }
+    else if(cmd_argc == 1)
+    {
+        // add code here
+        string msg;
+        cout << "Nhap ban tin:";
+        getline(cin, msg);
+        bool ok = client.echo(msg);
+        if(ok)
+            cout << "Echo thanh cong." << endl;
+    }
+    else
+    {
+        cout << "Invalid arguments" << endl;
+    }
+}
 
 void HttpClientCLI::doHelp(string cmd_argv[], int cmd_argc)
 {
     cout << "Please use the following commands:" << endl;
-    cout << "- open <server> [port]   Mo ket noi den Http server" << endl;
-    cout << "- close                   Dong ket noi" << endl;
-    cout << "- help                    Tro giup" << endl;
-    cout << "- quit                    Ket thuc chuong trinh" << endl;
-    cout << "- attendace <MSV>          Diem danh" << endl;
-    cout << "- upload <file link>       upload bai tap" << endl;
+    cout << "- open  hostname [port]    Mo ket noi den Echo server" << endl;
+    cout << "- echo [msg]               Gui va nhan ban tin" << endl;
+    cout << "- close                    Dong ket noi" << endl;
+    cout << "- help                     Tro giup" << endl;
+    cout << "- quit                     Ket thuc chuong trinh" << endl;
+    cout << "- get [URL]                Su dung method GET" << endl;
+    cout << "- post [URL] <MSV>         Su dung method POST" << endl;
 }
 
 
--- httptcpclient.cpp
+++ httptcpclient.cpp
@@ -1,6 +1,6 @@
 #include "httptcpclient.h"
 #include <iostream>
-using namespace std;
+#include <string>
 
 
 bool HttpTcpClientCLI::getHTTP (const string& url)
@@ -60,54 +60,114 @@ bool HttpTcpClientCLI::getHTTP (const string& url)
     return true;
 }
 
+bool HttpTcpClientCLI::postHTTP (const string& url, const string& msv)
+{
+    if(!isConnected())
+    {
+        cout << "Loi: chua co connection." << endl;
+        return false;
+    }
+    else
+    {
+        string httpRequest;
+        string body = "{\"student_id\": \"" + msv + "\"}";
+        int cLength= body.length();
+        // tao request theo format
+        httpRequest = "POST " + url + " HTTP/1.1\r\n";
+        httpRequest.append("Content-Type: application/json\r\n");
+        httpRequest.append("Host: 127.0.0.1:3000\r\n");
+        httpRequest.append("Content-Length: " + to_string(cLength) +"\r\n");
+        httpRequest.append("\r\n");
+        httpRequest.append(body);
+        int byte_sent = sendStringRequest(httpRequest);  // gui ban tin Echo request
+        if(byte_sent > 0) // neu gui thanh cong request
+        {
+            // chuan bi nhan phan hoi tu HTTP Server
+            string httpResponse;
+            int byte_recv = 0; // tong so byte nhan lai
+            int bytes; // so byte nhan trong 1 lan
+            char buffer[256]; // bo dem buffer kich thuoc 128 byte
 
-//bool EchoTcpClient::echo (const string& msg)
-//{
-//    if(!isConnected())
-//    {
-//        cout << "Loi: chua co connection." << endl;
-//        return false;
-//    }
-//    else
-//    {
-//        // add code here
-//        int byte_sent = sendStringRequest(msg);  // gui ban tin Echo request
-//        if(byte_sent > 0) // neu gui thanh cong request
-//        {
-//            string response;
-//            int byte_recv = 0; // tong so byte nhan lai
-//            int bytes; // so byte nhan trong 1 lan
-//            char buffer[128]; // bo dem buffer kich thuoc 128 byte
-//
-//            /* vong lap nhan phan hoi tu Echo Server */
-//            do
-//            {
-//                bytes = recvStringBuffer(buffer,sizeof(buffer)); // nhan 1 xau ki tu vao buffer
-//                if(bytes>0) // nhan duoc du lieu neu bytes > 0, neu khong thi error
-//                {
-//                    byte_recv += bytes; // tang so byte nhan duoc
-//                    response += buffer; // ghep xau nhan duoc trong buffer vao ket qua response
-//                }
-//            }
-//            while(bytes>0 && byte_recv < byte_sent);
-//
-//            if(byte_recv == byte_sent) // da nhan du? phan hoi tu Echo Server
-//            {
-//                cout << "Server: ";
-//                print(response);  // in ket qua phan hoi cua Echo Server
-//                return true;
-//            }
-//            else  // co loi xay ra (socket error)
-//            {
-//                cout << "Loi: nhan response." << endl;
-//                return false;
-//            }
-//        }
-//        else
-//        {
-//            cout << "Loi: gui request." << endl;
-//            return false;
-//        }
-//    }
-//}
-//
+            // vong lap nhan phan hoi tu Echo Server
+            do
+            {
+                bytes = recvStringBuffer(buffer,sizeof(buffer)); // nhan 1 xau ki tu vao buffer
+                if(bytes>0) // nhan duoc du lieu neu bytes > 0, neu khong thi error
+                {
+                    byte_recv += bytes; // tang so byte nhan duoc
+                    httpResponse += buffer; // ghep xau nhan duoc trong buffer vao ket qua response
+                }
+            }
+            while(bytes>0);
+
+            if(bytes==0)
+            {
+                cout <<"Server da dong ket noi" << endl;
+                print(httpResponse);
+            }
+            else
+            {
+                cout << "Loi: nhan response" << endl;
+            }
+
+
+        }
+        else
+        {
+            cout << "Loi: gui request" << endl;
+        }
+
+    }
+    return true;
+}
+
+bool HttpTcpClientCLI::echo (const string& msg)
+{
+    if(!isConnected())
+    {
+        cout << "Loi: chua co connection." << endl;
+        return false;
+    }
+    else
+    {
+        // add code here
+        int byte_sent = sendStringRequest(msg);  // gui ban tin Echo request
+        if(byte_sent > 0) // neu gui thanh cong request
+        {
+            string response;
+            int byte_recv = 0; // tong so byte nhan lai
+            int bytes; // so byte nhan trong 1 lan
+            char buffer[128]; // bo dem buffer kich thuoc 128 byte
+
+            /* vong lap nhan phan hoi tu Echo Server */
+            do
+            {
+                bytes = recvStringBuffer(buffer,sizeof(buffer)); // nhan 1 xau ki tu vao buffer
+                if(bytes>0) // nhan duoc du lieu neu bytes > 0, neu khong thi error
+                {
+                    byte_recv += bytes; // tang so byte nhan duoc
+                    response += buffer; // ghep xau nhan duoc trong buffer vao ket qua response
+                }
+            }
+            while(bytes>0 && byte_recv < byte_sent);
+
+            if(byte_recv == byte_sent) // da nhan du? phan hoi tu Echo Server
+            {
+                cout << "Server: ";
+                print(response);  // in ket qua phan hoi cua Echo Server
+                return true;
+            }
+            else  // co loi xay ra (socket error)
+            {
+                cout << "Loi: nhan response." << endl;
+                return false;
+            }
+        }
+        else
+        {
+            cout << "Loi: gui request." << endl;
+            return false;
+        }
+    }
+}
+
